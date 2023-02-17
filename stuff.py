19bd23ac1dcd78cb31fd12ef0e7e5eabc993383b
Anantjit <anantjitchander@gmail.com>
2023-02-17 18:52:00 +0530

    Add files via upload

--- stuff.py
+++ stuff.py
@@ -1,10 +1,18 @@
 import random
+import numpy as np
+
 
 LowestNumberRange=int(input("Please enter the lowest of number you want to get the questions from: "))
 HighestNumberRange=int(input("Please enter the highest of number you want to get the questions from: "))   
 QuestionsNumber=int(input("How many questions do you want? "))  
 QuestionArray=[]  
 Number=0
+score=0
+answer1=0
+answer2=0
+answer3=0
+answer4=0
+operator=""
 
 def generate_question():
     # Generate two random numbers for the base and exponent
@@ -22,16 +30,16 @@ def generate_question():
     # Generate a random question
     if operator == "+":
         question = f"What is the value of {base1}^{exponent1} + {base2}^{exponent2}?"
-        answer=base1**exponent1+base2**exponent2
+        answer1=base1**exponent1+base2**exponent2
     elif operator == "x":
         question = f"What is the value of {base1}^{exponent1} * {base2}^{exponent2}?"
-        answer=base1**exponent1*base2**exponent2
+        answer2=base1**exponent1*base2**exponent2
     elif operator == "÷":
         question = f"What is the value of {base1}^{exponent1} / {base2}^{exponent2}?"
-        answer=base1**exponent1/base2**exponent2
+        answer3=base1**exponent1/base2**exponent2
     else:
         question = f"What is the value of {base1}^{exponent1} - {base2}^{exponent2}?"
-        answer=base1**exponent1+base2**exponent2
+        answer4=base1**exponent1+base2**exponent2
 
 
     return question
@@ -44,5 +52,35 @@ for i in range(QuestionsNumber):
     QuestionArray.append(generate_question())
     print(Number, ". ", QuestionArray[i])
 
-    userAnswer=int(input("What is the answer to this?"))
+    userAnswer=int(input("What's the answer?"))
+    if operator=="+" and userAnswer == answer1:
+        print("That's the correct answer!")
+        score=score+1
+    else:
+        print("Better luck next time! The answer was ", answer1)
+
+    
+
+    if operator=="-" and userAnswer == answer2:
+        print("That's the correct answer!")
+        score=score+1
+    else:
+        print("Better luck next time! The answer was ", answer2)
+
+    
+
+    if operator=="x" and userAnswer == answer3:
+        print("That's the correct answer!")
+        score=score+1
+    else:
+        print("Better luck next time! The answer was ", answer3)
+
+    
+    if operator=="÷" and userAnswer == answer4:
+        print("That's the correct answer!")
+        score=score+1
+    else:
+        print("Better luck next time! The answer was ", answer4)
+
+    print("Your current score is ", score)
 
